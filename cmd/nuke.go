cfccafdb4f2377bba223bbc1c3aca9f51aca53fd
leow93 <leowheelan@hotmail.co.uk>
2025-04-23 15:53:01 +0100

    add confirmation to nuke cmd

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -1,12 +1,31 @@
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/leow93/todo/config"
 	"github.com/leow93/todo/todos"
 )
 
 func Nuke(cfg *config.Config, t *todos.Todos) {
+	fmt.Println("This action will delete all your todos. Are you absolutely sure? (y/n)")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	answer := input.Text()
+	if err := input.Err(); err != nil {
+		panic(err)
+	}
+
+	if strings.ToLower(answer) != "y" {
+		return
+	}
+
 	t.Nuke()
 
 	write(cfg, t)
+
+	fmt.Println("Todos deleted")
 }
